2324db3278923f53c6991cd0672792f3d376f648
Jason Stajich <jasonstajich.phd@gmail.com>
2022-07-19 13:22:24 -0700

    plot script

--- Pangenome_analysis/Subtelo_analysis/pipeline/02_plot.sh
+++ Pangenome_analysis/Subtelo_analysis/pipeline/02_plot.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash -l
 #SBATCH -p short --mem 8gb --out logs/rplot.log
 
-Rscript scripts/compare_subtelo_observations.R
+Rscript scripts/compare_subtelo_observations_100k.R
+Rscript scripts/compare_subtelo_observations_50k.R
